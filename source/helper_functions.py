a3c164bf71934f8c699df0620ecc8ee136a3c878
Carlos Alvarado (from VM) <cealvara@gmail.com>
2017-04-10 15:02:40 -0500

    changes to hw2

--- source/helper_functions.py
+++ source/helper_functions.py
@@ -1,5 +1,51 @@
+import pandas as pd
+
+# 1. Read Data: For this assignment, assume input is CSV and write a function 
+# that can read a csv into python
+
 def read_data(filename):
-    with open(filename) as f:
-        data = f.read()
+    data = pd.read_csv(filename)
 
     return data
+
+# 2. Explore Data: You can use the code you wrote for assignment 1 here to 
+# generate distributions and data summaries.
+
+def varnames(df):
+    print(df.columns)
+
+def explore(df):
+    df.describe()
+    pass
+
+# 3. Pre-Process Data: For this assignment, you can limit this to filling in 
+# missing values for the variables that have missing values. You can use any 
+# simple method to do it (use mean to fill in missing values).
+
+def fill_miss(df):
+    pass
+
+# 4. Generate Features/Predictors: For this assignment, you should write one 
+# function that can discretize a continuous variable and one function that can 
+# take a categorical variable and create binary/dummy variables from it. Apply 
+# them to at least one variable each in this data.
+
+def discretize(df, var):
+    pass
+
+def make_dummies(df, var):
+    pass
+
+# 5. Build Classifier: For this assignment, select any classifier you feel 
+# comfortable with (Logistic Regression for example)
+
+def classify(df, var):
+    pass
+# 6. Evaluate Classifier: you can use any metric you choose for this assignment 
+# (accuracy is the easiest one). Feel free to evaluate it on the same data you 
+# built the model on (this is not a good idea in general but for this assignment, 
+#     it is fine). We haven't covered models and evaluation yet, so don't worry 
+# about creating validation sets or cross-validation. 
+
+def evaluate(df, var):
+    pass
