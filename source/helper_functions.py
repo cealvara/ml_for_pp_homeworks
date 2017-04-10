20282907d2b4a736ac2de77b51b20c6da0e6dffa
Carlos Alvarado (from CS) <cealvara@gmail.com>
2017-04-10 16:28:06 -0500

    changes to hw2

--- source/helper_functions.py
+++ source/helper_functions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # 1. Read Data: For this assignment, assume input is CSV and write a function 
 # that can read a csv into python
@@ -30,11 +31,58 @@ def fill_miss(df):
 # take a categorical variable and create binary/dummy variables from it. Apply 
 # them to at least one variable each in this data.
 
-def discretize(df, var):
-    pass
+def discretize(df, varname, nbins, cut_type='quantile'):
+    '''
+    Discretizes given "varname" into "nbins".
 
-def make_dummies(df, var):
-    pass
+    Inputs:
+            - df: name of pandas DataFrame
+            - varname: name of variable to be discretized
+            - nbins: number of categories to create
+            - cut_type: can be 'quantile', 'uniform', 'linspace' or 'logspace'
+
+    Returns: nothing. Modifies "df" in place
+    '''
+    accepted_cut_types = ['quantile', 'uniform', 'linspace', 'logspace']
+
+    assert varname in df.columns, "Column '{}' not found in DataFrame".format(varname)
+
+    assert len(df[varname].value_counts()) > nbins, "Number of bins too large"
+
+    assert cut_type in accepted_cut_types, "Given cut_type not allowed"
+
+
+    if cut_type == 'quantile':
+        df[varname+'_cat'] = pd.qcut(df[varname], nbins)
+    elif cut_type == 'uniform':
+        df[varname+'_cat'] = pd.cut(df[varname], nbins)
+    elif cut_type == 'linspace':
+        minval = min(df[varname])
+        maxval = max(df[varname])
+        bins = np.linspace(minval, maxval, nbins+1)
+        df[varname+'_cat'] = pd.cut(df[varname], bins, include_lowest=True)
+    elif cut_type == 'logspace':
+        minval = min(df[varname])
+        maxval = max(df[varname])
+        
+        assert maxval > 0, 'Column {} has only negative or zero numbers'.format(varname)
+
+        if minval <= 0:
+            print('Warning, {} has negative or zero values'.format(varname))
+            minval = 0.0001
+
+        bins = np.logspace(np.log10(minval), np.log10(maxval), num = nbins+1)
+        df[varname+'_cat'] = pd.cut(df[varname], bins, include_lowest=True)
+
+
+def make_dummies(df, varname):
+    '''
+    Creates a set of dummies out of "varname"
+    '''
+    for i, value in enumerate(df[varname].unique()):
+        print(type(value), value)
+        df[varname+'_{}'.format(value)] = df[varname] == value
+
 
 # 5. Build Classifier: For this assignment, select any classifier you feel 
 # comfortable with (Logistic Regression for example)
