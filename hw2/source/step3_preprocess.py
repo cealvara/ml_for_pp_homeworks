0f01c3c7c4ed6081242d68c3fd7504aab9d50f57
Carlos Alvarado (from VM) <cealvara@gmail.com>
2017-04-17 16:20:55 -0500

    changes to hw2

---
+++ hw2/source/step3_preprocess.py
@@ -0,0 +1,6 @@
+# 3. Pre-Process Data: For this assignment, you can limit this to filling in 
+# missing values for the variables that have missing values. You can use any 
+# simple method to do it (use mean to fill in missing values).
+
+def fill_miss(df):
+    pass
---
+++ hw2/source/step5_classifiers.py
@@ -0,0 +1,7 @@
+
+
+# 5. Build Classifier: For this assignment, select any classifier you feel 
+# comfortable with (Logistic Regression for example)
+
+def classify(df, var):
+    pass
