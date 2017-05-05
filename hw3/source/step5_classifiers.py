5e989ded6e106369421c9842a216003bd7135b4d
Carlos (from machine) <cealvara@gmail.com>
2017-05-04 23:27:24 -0500

    Finished HW3

--- hw3/source/step5_classifiers.py
+++ hw3/source/step5_classifiers.py
@@ -1,6 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier, BaggingClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
+from sklearn.svm import SVC, LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -12,16 +12,18 @@ def make_model(method):
     if method == 'RF':
         model = RandomForestClassifier(n_estimators=50, n_jobs=-1)
     elif method == 'BA':
-        model = BaggingClassifier(DecisionTreeClassifier(max_depth=1), n_estimators = 5, max_samples=0.65, max_features=1.)
+        model = BaggingClassifier(DecisionTreeClassifier(max_depth=1, criterion='gini', min_samples_split=2, max_features='log2'), max_samples=0.65, max_features=1., n_jobs=-1)
     elif method == 'AB':
-        model = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), algorithm="SAMME", n_estimators=200)
+        model = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1, criterion='gini', min_samples_split=2, max_features='log2'))
     elif method == 'LR':
-        model = LogisticRegression(penalty='l1', C=1e5)
+        model = LogisticRegression(penalty='l1', C=1e5, n_jobs=-1)
     elif method == 'SVM':
         model = SVC(kernel='linear', probability=True, random_state=0)
+    elif method == "LSVM":
+        model = LinearSVC(penalty='l2', random_state=0)
     elif method == 'DT':
         model = DecisionTreeClassifier()
     elif method == 'KNN':
-        model = KNeighborsClassifier(n_neighbors=3)
+        model = KNeighborsClassifier(n_neighbors=3, n_jobs=-1)
 
     return model
